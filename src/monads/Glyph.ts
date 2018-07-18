2145a299a206cb2b520b43049b4d27e240b9da94
danlugo92 <danlugo92@gmail.com>
2018-07-18 09:13:27 -0400

    work

--- src/monads/Glyph.ts
+++ src/monads/Glyph.ts
@@ -17,7 +17,7 @@ class _Glyph extends BaseMonad {
 
   matches(tokens: Set<Token>): boolean {
     return reduce(tokens, (str, nxtToken) => str + nxtToken, '') === this.of
-  }  
+  }
 }
 
 export default (of: string, name?: string): Type => new _Glyph(of, name)
